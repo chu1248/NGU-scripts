8b404d309599d39a4201bc8b1e0cda92c027c1d8
Chris <lsccly@gmail.com>
2019-12-08 23:58:56 +0800

    After farmed lv1 of badly drawn world

--- rebirth_24hr.py
+++ rebirth_24hr.py
@@ -20,13 +20,14 @@ Helper.requirements()
 
 curState = ""
 
-zone_after_rebirth = 14
-zone_after_training = 16
-deadline_before_adv_training = time.strptime("00:25:00", "%H:%M:%S")
-deadline_train_power_toughness = time.strptime("01:30:00", "%H:%M:%S")
-deadline_augmentation = time.strptime("01:45:00", "%H:%M:%S")  # deadline_train_power_toughness.tm + datetime.timedelta(minutes=15)
-deadline_adv_wandoos = time.strptime("02:00:00", "%H:%M:%S")  # deadline_augmentation + datetime.timedelta(minutes=15)
-deadline_wandoos = time.strptime("02:15:00", "%H:%M:%S")  # deadline_adv_wandoos + datetime.timedelta(minutes=15)
+zone_after_rebirth = 16  # Beardverse
+zone_after_training = 18  # badly drawn world
+deadline_before_adv_training = time.strptime("00:13:00", "%H:%M:%S")
+deadline_train_power_toughness = time.strptime("01:15:00", "%H:%M:%S")
+deadline_augmentation = time.strptime("01:30:00", "%H:%M:%S")  # deadline_train_power_toughness.tm + datetime.timedelta(minutes=15)
+deadline_adv_wandoos = time.strptime("01:40:00", "%H:%M:%S")  # deadline_augmentation + datetime.timedelta(minutes=15)
+deadline_wandoos = time.strptime("02:00:00", "%H:%M:%S")  # deadline_adv_wandoos + datetime.timedelta(minutes=15)
+deadline_gold = time.strptime("02:40:00", "%H:%M:%S")  # for wear gold drop equip for gold drop from titan
 
 #Adventure.snipe(zone=4, duration=60, manual=True, fast=True)  # snipe for macggufin
 #exit(1)
@@ -56,7 +57,8 @@ while True:
                 print(f"E: {e_idle} \t M: {m_idle}")
 
             Inventory.loadout(gold_loadout)
-        TimeMachine.time_machine(e=Misc.get_idle_cap(1), m=Misc.get_idle_cap(2))  # need to keep adding as filling up
+        if Misc.get_idle_cap(1) > 0 or Misc.get_idle_cap(2) > 0:
+            TimeMachine.time_machine(e=Misc.get_idle_cap(1), m=Misc.get_idle_cap(2))  # need to keep adding as filling up
         FightBoss.nuke()
         Adventure.snipe(zone=zone_after_rebirth, duration=1, once=True, bosses=True, manual=False)  # auto to be safe
         Adventure.itopod_snipe(60, auto=True)  # may not have skill yet
@@ -80,9 +82,10 @@ while True:
                 print(f"E: {Misc.get_idle_cap(1)} \t M: {Misc.get_idle_cap(2)}")
 
             Inventory.loadout(ngu_loadout)
+        if Misc.get_idle_cap(1) > 0 or Misc.get_idle_cap(2) > 0:
             # Focus on adv training power and toughness
-            AdvancedTraining.advanced_training(value=(Misc.get_idle_cap(1) // 2), ability=1)  # Toughness
-            AdvancedTraining.advanced_training(value=Misc.get_idle_cap(1), ability=2)  # Power
+            AdvancedTraining.advanced_training(energy=(Misc.get_idle_cap(1) // 2), ability=1)  # Toughness
+            AdvancedTraining.advanced_training(energy=Misc.get_idle_cap(1), ability=2)  # Power
             # Continue on magic time machine
             TimeMachine.time_machine(e=0, m=Misc.get_idle_cap(2))
         # Snipe itopod
@@ -107,12 +110,13 @@ while True:
                 print(f"E: {Misc.get_idle_cap(1)} \t M: {Misc.get_idle_cap(2)}")
 
             Inventory.loadout(gold_loadout)
+
+        if Misc.get_idle_cap(1) > 0 or Misc.get_idle_cap(2) > 0:
             # Augmentation
-            Augmentation.augments({"CI": 0.7, "ML": 0.3}, Misc.get_idle_cap(1))
+            Augmentation.augments({"CI": 0.34, "ML": 0.66}, Misc.get_idle_cap(1))
             # Blood magic for gold
             BloodMagic.toggle_auto_spells(number=False, drop=False, gold=True)
             BloodMagic.blood_magic_reverse(blood_magic_highest_affordable_level)
-
         Adventure.snipe(zone=zone_after_training, duration=1, once=True, bosses=True, manual=True)
         Adventure.itopod_snipe(60)
 
@@ -134,11 +138,12 @@ while True:
                 print(f"E: {Misc.get_idle_cap(1)} \t M: {Misc.get_idle_cap(2)}")
 
             Inventory.loadout(ngu_loadout)
+        if Misc.get_idle_cap(1) > 0 or Misc.get_idle_cap(2) > 0:
             # Adv training for wandoos
-            AdvancedTraining.advanced_training(value=(Misc.get_idle_cap(1) // 2), ability=4)  # wandoos energy
-            AdvancedTraining.advanced_training(value=Misc.get_idle_cap(1), ability=5)  # wandoos magic
-            # Blood magic for number
-            BloodMagic.toggle_auto_spells(number=True, drop=False, gold=False)
+            AdvancedTraining.advanced_training(energy=(Misc.get_idle_cap(1) // 2), ability=4)  # wandoos energy
+            AdvancedTraining.advanced_training(energy=Misc.get_idle_cap(1), ability=5)  # wandoos magic
+            # Blood magic for drop
+            BloodMagic.toggle_auto_spells(number=False, drop=True, gold=False)
             BloodMagic.blood_magic_reverse(blood_magic_highest_affordable_level)
         # enemies
         Adventure.itopod_snipe(60)
@@ -161,14 +166,45 @@ while True:
                 print(f"E: {Misc.get_idle_cap(1)} \t M: {Misc.get_idle_cap(2)}")
 
             Inventory.loadout(ngu_loadout)
+            GoldDiggers.deactivate_all_diggers()
+            GoldDiggers.gold_diggers(targets=[2])  # wandoos
+
+        if Misc.get_idle_cap(1) > 0 or Misc.get_idle_cap(2) > 0:
             # Wandoos
             Wandoos.wandoos(energy=True, magic=True)
+            # Blood magic for number
+            BloodMagic.toggle_auto_spells(number=True, drop=False, gold=False)
+            BloodMagic.blood_magic_reverse(blood_magic_highest_affordable_level)
+
             TimeMachine.time_machine(e=Misc.get_idle_cap(1),
                                      m=Misc.get_idle_cap(2))
-            # Blood magic for drop
-            # BloodMagic.toggle_auto_spells(number=False, drop=True, gold=False)
-            # BloodMagic.blood_magic_reverse(blood_magic_highest_affordable_level)
 
+        # enemies
+        Adventure.itopod_snipe(60)
+
+    elif rt.days == 0 and rt.timestamp < deadline_gold:
+        stateName = "Gold"
+        if curState != stateName:
+            print(stateName)
+            curState = stateName
+
+            if debug:
+                print(f"E: {Misc.get_idle_cap(1)} \t M: {Misc.get_idle_cap(2)}")
+                TimeMachine.time_machine(e=Misc.get_idle_cap(1),
+                                         m=Misc.get_idle_cap(2))  # force away the resource
+            Misc.reclaim_all()
+            if debug:
+                while Misc.get_idle_cap(1) == 0 or Misc.get_idle_cap(2) == 0:
+                    print(f"E: {Misc.get_idle_cap(1)} \t M: {Misc.get_idle_cap(2)}")
+                    Misc.reclaim_all()
+                print(f"E: {Misc.get_idle_cap(1)} \t M: {Misc.get_idle_cap(2)}")
+
+            Inventory.loadout(gold_loadout)
+            GoldDiggers.deactivate_all_diggers()
+            GoldDiggers.gold_diggers(targets=[4, 1], deactivate=True)  # add adv stats
+        if Misc.get_idle_cap(1) > 0 or Misc.get_idle_cap(2) > 0:
+            TimeMachine.time_machine(e=Misc.get_idle_cap(1),
+                                     m=Misc.get_idle_cap(2))  # need to keep adding as filling up
         # enemies
         Adventure.itopod_snipe(60)
 
@@ -182,7 +218,7 @@ while True:
             Yggdrasil.ygg(equip=1)  # harvest with equipment set 1
             Yggdrasil.ygg(eat_all=True)
             GoldDiggers.level_diggers()  # level all diggers
-            GoldDiggers.gold_diggers(targets=[3])  # stat digger for stat
+            GoldDiggers.gold_diggers(targets=[3])  # cap for stat
             FightBoss.nuke()
             MoneyPit.spin()
             MoneyPit.pit()
@@ -208,12 +244,15 @@ while True:
                 print(f"E: {Misc.get_idle_cap(1)} \t M: {Misc.get_idle_cap(2)}")
 
             Inventory.loadout(ngu_loadout)
-            NGU.assign_ngu(value=Misc.get_idle_cap(1), targets=(list(range(1, 10)) + [5] * 9), magic=False)
-            NGU.assign_ngu(value=Misc.get_idle_cap(2), targets=(list(range(1, 8)) + [1, 2, 5, 7]), magic=True)
             GoldDiggers.deactivate_all_diggers()
-            GoldDiggers.gold_diggers([5, 6], deactivate=True)  # Energy NGU, Magic NGU
+            GoldDiggers.gold_diggers([5, 6, 4, 1], deactivate=True)  # Energy NGU, Magic NGU, Adventure Stat, drop
+        if Misc.get_idle_cap(1) > 20:
+            NGU.assign_ngu(value=Misc.get_idle_cap(1), targets=(list(range(1, 10)) + [5] * 9), magic=False)
+        if Misc.get_idle_cap(2) > 20:
+            NGU.assign_ngu(value=Misc.get_idle_cap(2), targets=(list(range(1, 8)) + [5] + [7] * 7), magic=True)
         #Adventure.snipe(zone=zone_after_training, duration=1, once=True, bosses=True, manual=True)
         Adventure.itopod_snipe(60)
+        #Adventure.snipe(zone=zone_after_training, duration=1, once=False, bosses=True, manual=True)
 
     # common area
     FightBoss.nuke()
